cf0a174acf1a802cd0fa1ecdb3e5fea18df0709b
Youri Bontekoe <youribontekoe@gmail.com>
2026-02-22 15:48:57 +0100

    Delete verify_admin.ts

--- verify_admin.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('admin login page renders', async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
-  await expect(page.locator('h2')).toContainText('Valora Admin');
-  await expect(page.locator('button')).toContainText('Login');
-});
